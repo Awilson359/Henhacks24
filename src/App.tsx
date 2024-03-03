f479b476d172d26a0d8a4af367b17597381a44c9
Awilson359 <alexisswilson@gmail.com>
2024-03-02 19:44:58 -0500

    7:44

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import AtomDrawing from './components/AtomDrawing'
+import UserInput from './components/UserInput'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
     <>
-      <AtomDrawing element = {}/>
+      <AtomDrawing />
+      <UserInput />
     </>
   )
 }
 
-export default App
+export default App;
