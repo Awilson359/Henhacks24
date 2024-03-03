c8510608c0b69b2f60cc03719d44ffabed5f4b4b
HenryLeap <henrywleap@gmail.com>
2024-03-02 17:46:15 -0800

    First attempt UserInput

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -1,6 +1,16 @@
 function UserInput(){
+    
     return (
-
+<>
+    <label>
+        Amount of Protons:
+        <input name = "protonNum" defaultValue="0"/>
+        Amount of Neutrons:
+        <input name = "neutronNum" defaultValue="0"/>
+        Amount of Electrons:
+        <input name = "electronNum" defaultValue="0"/>
+    </label>
+</>
     )
 }
 
