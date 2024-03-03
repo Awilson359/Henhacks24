0ce65a4c8c6a1a6406e0cababfbb7c82444e1498
starrytraitor <ssszhang05@gmail.com>
2024-03-02 19:37:13 -0500

    I MADE A CIRCLEEEEEE

--- src/components/AtomDrawing.tsx
+++ src/components/AtomDrawing.tsx
@@ -1,5 +1,28 @@
-function AtomDrawing(){
-    return ()
+import Circle from "./Circle";
+// props.element.protons
+
+function AtomDrawing(props){
+    const nucleus: Circle = <Circle bgColor={"#F4E310"}/>
+    
+    return (
+        <>
+        <style>
+            
+        </style>
+        
+        <html>
+            <head>
+                
+            </head>
+            
+            <body>
+                <div id = "container">
+
+                </div>
+            </body>
+        </html>
+        </>
+    )
 }
 
 export default AtomDrawing;
