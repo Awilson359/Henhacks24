aed76a7d8b65c88c4b70225795a4d63f4f1f7f5f
starrytraitor <ssszhang05@gmail.com>
2024-03-02 21:25:49 -0500

    margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-=======
-import Circle from './components/Circle'
->>>>>>> 42f29dbe29e3c929c2dd19378268166510e61c4d
 import './App.css'
 import Circle from './components/Circle'
 import AtomDrawing from './components/AtomDrawing'
@@ -10,26 +6,13 @@ import UserInput from './components/UserInput'
 
 function App() {
   const [count, setCount] = useState(0)
-<<<<<<< HEAD
 
   return (
     <>
       <UserInput />
       <AtomDrawing />
-      <Circle bgColor = "#000000"/>
-=======
-  
-  return (
-    <>
-      <AtomDrawing />
-      <UserInput />
->>>>>>> 42f29dbe29e3c929c2dd19378268166510e61c4d
     </>
   )
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 42f29dbe29e3c929c2dd19378268166510e61c4d
