0df499dfdabd656b0465e66479b45f682e814431
starrytraitor <ssszhang05@gmail.com>
2024-03-02 22:27:52 -0500

    ????????

--- src/components/AtomDrawing.tsx
+++ src/components/AtomDrawing.tsx
@@ -1,11 +1,21 @@
 import Circle from "./Circle";
 import Ring from "./Ring";
+import Elements from "../Elements";
+import { useState } from "react";
 // props.element.protons
 
 function AtomDrawing(props){
     const nucleus: Circle = <Circle bgColor={"#F4E310"}/>
-    const shell1: Ring = <Ring diameter = {200}/>
-    const shell2: Ring = <Ring diameter = {300}/>
+    const shells = [];
+    const [diam, setDiam] = useState(200);
+    
+    for (let i = 0; i < props.Elements.shells; i++) {
+        shells.push(<Ring diameter = {diam}/>)
+        setDiam(diam+100);
+    }
+    const shelements = shells.map( shell => {
+        return shell;
+    })
 
     return (
         <>
@@ -16,13 +26,13 @@ function AtomDrawing(props){
             
             <body>
                 <div id = "container">
-                    {shell1}
-                    {shell2}
+                    {shelements}
                     {nucleus}
                 </div>
                 <div id = "containter">
-                    
+
                 </div>
+                {Elements[0].name}
             </body>
         </html>
         </>
